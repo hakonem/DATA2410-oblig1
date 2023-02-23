9704abf91a58b1bbbae2e7bfb8561ad112dc2358
emmah <emma.hakonsen@gmail.com>
2023-02-23 11:05:18 +0100

    Task 2: Get 200 response from server when host/port/object passed directly into GET request

--- task1.py
+++ task1.py
@@ -12,18 +12,19 @@ while True:
     print('Ready to serve...')
     connectionSocket,addr = serverSocket.accept()       #accept new connection request from client
     try:
-        message = connectionSocket.recv(2048)           #receive request message from client
+        message = connectionSocket.recv(4096)           #receive request message from client
+        print(message)
         filename = message.split()[1]                   #extract path to requested object (second element in request line)
-        f = open(filename[1:])                      #read path from second character (ignore initial '\')
-        outputdata = f.read()                   #read file and store contents in outputdata
-        f.close()                               #close file
+        f = open(filename[1:])                          #read path from second character (ignore initial '\')
+        outputdata = f.read()                           #read file and store contents in outputdata
+        f.close()                                       #close file
 
         connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())    #Send one HTTP header line (if request successful) into socket
 
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
-        connectionSocket.send("\r\n".encode())
+        connectionSocket.send('\r\n'.encode())
         connectionSocket.close()
 
     except IOError:
--- task2.py
+++ task2.py
@@ -1,6 +1,4 @@
 import socket
-import sys
-import requests
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -9,11 +7,8 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #serverPort = 18881
 client_socket.connect(('127.0.0.1',18881))
 
-while True:
-    url = "http://192.168.50.207:18881/index.html"
-    request = requests.request("GET", url)
-    #client_socket.send(b"GET / HTTP/1.1\r\nHost:192.168.50.207\r\n\r\n")
-    response = client_socket.recv(2048)
+client_socket.send(b'GET /index.html HTTP/1.1\r\nHost: 127.0.0.1:18881\r\n\r\n')
+response = client_socket.recv(4096)
 
 client_socket.close()
 print(response.decode())
