deedf3d54ad12972977a0e691eb9a8ec3da7ba47
emmah <emma.hakonsen@gmail.com>
2023-02-23 12:44:12 +0100

    Updated task 2 so that it now returns an object based on user input

--- task2.py
+++ task2.py
@@ -1,4 +1,5 @@
 import socket
+import sys
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -7,8 +8,12 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #serverPort = 18881
 client_socket.connect(('127.0.0.1',18881))
 
-client_socket.send(b'GET /index.html HTTP/1.1\r\nHost: 127.0.0.1:18881\r\n\r\n')
-response = client_socket.recv(4096)
-
-client_socket.close()
-print(response.decode())
+while True:
+    input = sys.stdin.readline()              #input command format: host port filename
+    #request = 'GET /index.html HTTP/1.1\r\nHost: 127.0.0.1:18881\r\n\r\n'
+    request = 'GET /' + input.split()[2] + ' HTTP/1.1\r\nHost: ' + input.split()[0] + ':' + input.split()[1] + '\r\n\r\n'
+    print(request)
+    client_socket.send(request.encode())
+    response = client_socket.recv(4096)
+    client_socket.close()
+    print(response.decode())
