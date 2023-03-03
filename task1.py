1f52d01ccc15e2e5712edfdfb1b7c7d66a69a061
emmah <emma.hakonsen@gmail.com>
2023-03-03 11:40:06 +0100

    Task 1 updated with full commentary

--- task1.py
+++ task1.py
@@ -1,33 +1,34 @@
-#import socket module
+#import socket and sys modules
 from socket import *
 import sys
 
-serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket = socket(AF_INET, SOCK_STREAM)             #prepare a TCP (SOCK_STREAM) server socket using IPv4 (AF_INET)
+serverSocket.bind(('',18881))                           #bind server socket to local address ('' = all available addresses) and port
+serverSocket.listen(1)                                  #listen for incoming connection requests to socket (no more than 1 at a time)
+print('Ready to serve...')                              #print message if socket correctly set up
 
-#Prepare a server socket
-serverSocket.bind(('',18881))       #bind socket to server address and port
-serverSocket.listen(1)              #listen for connection to socket
-print('Ready to serve...')
 while True:
-    connectionSocket,addr = serverSocket.accept()       #accept new connection request from client
+    connectionSocket,addr = serverSocket.accept()       #accept connection request from client and create new connection socket with info about the client (addr)
     try:
-        message = connectionSocket.recv(4096)           #receive request message from client
-        print(message)
-        filename = message.split()[1]                   #extract path to requested object (second element in request line)
-        f = open(filename[1:])                          #read path from second character (ignore initial '\')
-        outputdata = f.read()                           #read file and store contents in outputdata
+        message = connectionSocket.recv(2048)           #receive GET request from client (max 2048 bytes), save to 'message' variable
+        print(message)                                  #GET request printed to screen server side
+        #GET request arrives in following format: GET <path to file> HTTP/1.1\r\nHost: <host>\r\n\r\n
+        filename = message.split()[1]                   #extract path to requested file (second element in request line), save to 'filename' variable
+        f = open(filename[1:])                          #read path from second character (ignores initial '\'), save to 'f' variable
+        outputdata = f.read()                           #read file and store contents in 'outputdata' variable
         f.close()                                       #close file
 
-        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())    #Send one HTTP header line (if request successful) into socket
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())    #If file is found, HTTP header line is sent to from connection socket to client
 
-        #Send the content of the requested file to the client
+        #Contents of the requested file sent to the client line by line in a for-loop
         for i in range(0, len(outputdata)):
             connectionSocket.send(outputdata[i].encode())
         connectionSocket.send('\r\n'.encode())
-        connectionSocket.close()
+        connectionSocket.close()                        #close connection socket once HTML file is sent
 
+    #EXCEPTION HANDLING
     except IOError:
-            connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())     #Send response message for file not found
+            connectionSocket.send('HTTP/1.1 404 Not Found\r\n\r\n'.encode())     #If the file is not found, 404 message is returned instead
 
     connectionSocket.close()                #Close client socket
 serverSocket.close()                        #Close server socket
